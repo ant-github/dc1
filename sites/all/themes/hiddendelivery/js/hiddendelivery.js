5591bca6e443296453c64d68fada42c1201325c4
George Brook <georgebrook@gmail.com>
2014-08-20 16:22:11 +0100

    better way of temporarily removing the extension notifier

--- sites/all/themes/hiddendelivery/js/hiddendelivery.js
+++ sites/all/themes/hiddendelivery/js/hiddendelivery.js
@@ -52,11 +52,10 @@
 	}
 
   // Extension notifier
-  $(window).on("load", function(){
-    if (!$("div#extensionInstalled").length) {
-      $('<div id="extension-notifier"><p>Have you got the extension?<div class="install-extension-button"><a class="btn btn-primary form-submit install-extension-button" href="https://chrome.google.com/webstore/detail/delivery-code-wishlist/jfoemdhjganhhccomidjodmikemlkajo" id="edit-submit" name="op" value="submit">Add to browser</a></div></p></div>').prependTo($('body')).slideDown(1000);
-    }
-    console.log($("div#extensionInstalled").length);
-  })
+  // $(window).on("load", function(){
+  //   if (!$("div#extensionInstalled").length) {
+  //     $('<div id="extension-notifier"><p>Have you got the extension?<div class="install-extension-button"><a class="btn btn-primary form-submit install-extension-button" href="https://chrome.google.com/webstore/detail/delivery-code-wishlist/jfoemdhjganhhccomidjodmikemlkajo" id="edit-submit" name="op" value="submit">Add to browser</a></div></p></div>').prependTo($('body')).slideDown(1000);
+  //   }
+  // })
 
 })(jQuery);
