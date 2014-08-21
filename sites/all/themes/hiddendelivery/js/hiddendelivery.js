ea0659004237c9f707398cae3d06a86f59eb8bad
George Brook <georgebrook@gmail.com>
2014-08-21 14:45:20 +0100

    fixing slider

--- sites/all/themes/hiddendelivery/js/hiddendelivery.js
+++ sites/all/themes/hiddendelivery/js/hiddendelivery.js
@@ -51,7 +51,7 @@
 		}
 	}
 
-  Extension notifier
+  //Extension notifier
   $(window).on("load", function(){
     if (!$("div#extensionInstalled").length) {
       $('<div id="extension-notifier"><p>Have you got the extension?<div class="install-extension-button"><a class="btn btn-primary form-submit install-extension-button" href="https://chrome.google.com/webstore/detail/delivery-code-wishlist/jfoemdhjganhhccomidjodmikemlkajo" id="edit-submit" name="op" value="submit">Add to browser</a></div></p></div>').prependTo($('body')).slideDown(1000);
