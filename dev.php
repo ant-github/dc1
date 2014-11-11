87aa1de72499edda51b1c15c2ee8aacc0dd436bd
rod <rod@zoocha.com>
2014-11-11 19:48:40 +0000

    endpoint

--- dev.php
+++ dev.php
@@ -11,55 +11,105 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 function address(){
   require_once DRUPAL_ROOT . '/sites/all/modules/contrib/addressfield/plugins/format/address.inc';
   $format = array();
-  $address = array('country' => 'US');
+  $country = 'GB';
+  $address = array('country' => $country);
   addressfield_format_address_generate($format, $address, array());
 
-  dpm($format, 'format');
+  //dpm($format, 'format');
 
   $keys = array_keys($format['street_block']);
 
-  dpm($keys);
+  //dpm($keys, 'keys');
 
   foreach ($keys as $key => $value) {
-
     if (strpos($value, '#') === FALSE ) {
-      $street_block[] = $format['street_block'][$value];
+      $street_block[$value][] = $format['street_block'][$value];
     }
   }
 
-  dpm($street_block);
 
   $keys = array_keys($format['locality_block']);
 
   foreach ($keys as $key => $value) {
 
     if (strpos($value, '#') === FALSE ) {
-      $locality_block[] = $format['locality_block'][$value];
+      $locality_block[$value] = $format['locality_block'][$value];
     }
   }
 
-  dpm($locality_block);
-}
+  $address = array_merge($street_block, $locality_block);
 
-function register(){
-  $fields = array(
-    'name' => 'user_name',
-    'mail' => 'user_name@example.com',
-    'pass' => '123',
-    'status' => 1,
-    'init' => 'email address',
-    'roles' => array(
-      DRUPAL_AUTHENTICATED_RID => 'authenticated user',
-    ),
-  );
-
-  foreach (field_info_instances('user', 'user') as $field_name => $instance) {
-    $fields[$field_name][LANGUAGE_NONE][0]['value'] = 'abc';
+    //clean values for better output
+  foreach ($address as $key => $value) {
+    unset($value[0]['#tag']);
+    unset($value[0]['#title']);
+    unset($value[0]['#attributes']);
+    unset($value[0]['#size']);
+    unset($value[0]['#prefix']);
+    unset($value[0]['#weight']);
+    unset($value['#tag']);
+    unset($value['#title']);
+    unset($value['#attributes']);
+    unset($value['#size']);
+    unset($value['#prefix']);
+    unset($value['#weight']);
+    unset($value['#suffix']);
+    $clean_address[$key] = $value;
   }
-dpm($fields);
-  //$account = user_save('', $fields);
 }
 
- register();
+//address();
+function create(){
+  // Adds backwards compatability with regression fixed in #1083242
+  $account = array();
+  $account['name'] = 'fuckmemeemem';
+  $account['mail'] = '45asasa34@testte.com';
+  $account['pass'] = 'tet';
+  $account['status'] = '1';
+  $account['field_delivery_address']['und'][0] = array(
+                            'element_key' => 'user|user|field_delivery_address|und|0',
+                            'thoroughfare' => 'test',
+                            'premise' => 'tes',
+                            'locality' => 'test',
+                            'administrative_area' => 'test',
+                            'postal_code' => 'tby3000',
+                            'country' => 'VA',
+                    );
+  $account['field_name']['und'][0]['value'] = 'fuckser';
+  dpm($account);
+  user_save(NULL, $account);
+  // Load the required includes for saving profile information
+  // with drupal_form_submit().
+  module_load_include('inc', 'user', 'user.pages');
+
+  // register a new user
+  $form_state['values'] = $account;
+  $form_state['values']['pass'] = array(
+    'pass1' => $account['pass'],
+    'pass2' => $account['pass'],
+  );
+  $form_state['values']['op'] = t('Create new account');
+  dpm($form_state);
+  // execute the register form
+  //drupal_form_submit('user_register_form', $form_state);
+  // find and store the new user into the form_state
+  if(isset($form_state['values']['uid'])) {
+    $form_state['user'] = user_load($form_state['values']['uid']);
+  }
+
+  // // Error if needed.
+  // if ($errors = form_get_errors()) {
+  //   return services_error(implode(" ", $errors), 406, array('form_errors' => $errors));
+  // }
+  // else {
+  //   $user = array('uid' => $form_state['user']->uid);
+  //   if ($uri = services_resource_uri(array('user', $user['uid']))) {
+  //     $user['uri'] = $uri;
+  //   }
+  // }
+  //   //return $user;
+}
+create();
+
 
 menu_execute_active_handler();
