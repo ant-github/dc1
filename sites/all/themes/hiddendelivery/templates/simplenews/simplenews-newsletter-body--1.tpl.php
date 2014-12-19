8ff2f1660e5790c1db1afffe86234a15acbb2038
rod <rod@zoocha.com>
2014-12-19 17:39:20 +0000

    footer tpl for newsletter

--- sites/all/themes/hiddendelivery/templates/simplenews/simplenews-newsletter-body--1.tpl.php
+++ sites/all/themes/hiddendelivery/templates/simplenews/simplenews-newsletter-body--1.tpl.php
@@ -27,4 +27,191 @@
  * @see template_preprocess_simplenews_newsletter_body()
  */
 ?>
-<?php print render($build); ?>
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;" width="100%">
+  <tbody>
+    <tr>
+      <td bgcolor="#FFFFFF" style="border-collapse: collapse;" valign="top">
+        <!--[if (gte mso 9)|(IE)]>
+          <table width="600" align="center" cellpadding="0" cellspacing="0" border="0">
+            <tr>
+              <td>
+                <![endif]-->
+        <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" class="content" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;max-width: 600px;width: 100%;">
+          <tbody>
+            <tr>
+              <td align="center" bgcolor="#000000" style="border-collapse: collapse;" valign="top">
+                <span style="color: #ffffff; font-family: Arial; font-size: 11px; text-align: center;">To ensure you receive our emails, please add email@domain.com to your address book or safe list </span></td>
+            </tr>
+            <tr>
+              <td style="padding: 10px 10px 10px 10px;border-collapse: collapse;" valign="top">
+                <!--[if (gte mso 9)|(IE)]>
+                        <table width="438" align="left" cellpadding="0" cellspacing="0" border="0">
+                          <tr>
+                            <td>
+                              <![endif]--><!-- Start Header-->
+                <table align="center" border="0" cellpadding="0" cellspacing="0" class="deviceWidth" width="580">
+                  <tbody>
+                    <tr>
+                      <td bgcolor="#ffffff" width="100%">
+                        <!-- Logo -->
+                        <table align="left" border="0" cellpadding="0" cellspacing="0" class="deviceWidth">
+                          <tbody>
+                            <tr>
+                              <td class="center" style="padding:10px 20px">
+                                <a href="#"><img alt="" border="0" src="http://gallery.mailchimp.com/be5f319b883fe2a2b83f96a61/images/29cd4659-94ad-4b7c-aee4-8459ac94a2f9.jpg" /></a></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <!-- End Logo -->
+                        <table align="left" border="0" cellpadding="0" cellspacing="0" class="deviceWidth">
+                          <tbody>
+                            <tr>
+                              <td class="center" style="padding: 30px 0 0 30px">
+                                <a href="#"><img alt="DeliveryCode available on the App Store" src="https://gallery.mailchimp.com/be5f319b883fe2a2b83f96a61/images/adde9ea1-f675-4a49-b4fb-517ccae6f418.jpg" /></a></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <!-- Nav -->
+                        <table align="right" border="0" cellpadding="0" cellspacing="0" class="deviceWidth">
+                          <tbody>
+                            <tr>
+                              <td class="center" style="font-size: 12px; color: #000000; font-weight: bold; text-align: right; font-family: Arial; line-height: 20px; vertical-align: middle; padding:40px 10px 20px; font-style:bold">
+                                <a href="http://www.deliverycode.com/" style="text-decoration: none; color: #000000;">Home</a> &nbsp;&nbsp;&nbsp; <a href="http://www.deliverycode.com/how-it-works" style="text-decoration: none; color: #000000;">How it Works</a> &nbsp;&nbsp;&nbsp; <a href="http://www.deliverycode.com/user" style="text-decoration: none; color: #000000;">My Account</a></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <!-- End Header --><!--[if (gte mso 9)|(IE)]> </td>
+                          </tr>
+                        </table>
+                        <![endif]--></td>
+                    </tr>
+                    <tr>
+                      <td style="border-collapse: collapse;" valign="top">
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;" width="100%">
+                          <tbody>
+                            <tr>
+                              <td style="border-collapse: collapse;" valign="top">
+                                <table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;" width="100%">
+                                  <tbody>
+                                    <tr>
+                                      <td align="center" style="border-collapse: collapse;max-width: 487px;width: 100%;background:#000000;" valign="top">
+                                        <p style="mso-table-lspace:0;mso-table-rspace:0; margin:0"><br />
+                                          <br />
+                                          <span style="color: #FFFFFF; font-family: Arial; font-size: 25px; line-height: 16px; font-weight:bold; padding: 0 0 10px 0;">YOU HAVE BEEN SPECIALLY SELECTED</span><br />
+                                          <br />
+                                          &nbsp;</p>
+                                      </td>
+                                    </tr>
+                                    <tr>
+                                      <td style="height: 10px;border-collapse: collapse;" valign="top">
+                                        &nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                      <td style="border-collapse: collapse;" valign="top">
+                                        <!--[if (gte mso 9)|(IE)]>
+                                        <table width="600" align="right" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid #ffffff;">
+                                          <tr>
+                                            <td>
+                                              <![endif]-->
+                                        <table align="left" border="0" cellpadding="0" cellspacing="0" class="main-text" style="border: 1px solid #ffffff;width: 100%;border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;">
+                                          <tbody>
+                                            <tr>
+                                              <td style="width: 20px;border-collapse: collapse;" valign="top">
+                                                &nbsp;</td>
+                                              <td style="border-collapse: collapse;" valign="top">
+                                                <table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;" width="100%">
+                                                  <tbody>
+                                                    <tr>
+                                                      <td style="border-collapse: collapse;" valign="top">
+                                                        <!--[if (gte mso 9)|(IE)]>
+                                                              <table width="580" align="right" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid #ffffff;">
+                                                                <tr>
+                                                                  <td>
+                                                                    <![endif]-->
+                                                        <table align="left" border="0" cellpadding="0" cellspacing="0" class="main-text" style="border: 1px solid #ffffff;width: 100%;border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;">
+                                                          <tbody>
+                                                            <tr>
+                                                              <td style="border-collapse: collapse;" valign="top">
+                                                                    <?php print render($build); ?>
+                                                              </td>
+                                                            </tr>
+
+                                                            <tr>
+                                                              <td align="center" style="border-collapse: collapse;" valign="top">
+                                                                <a alt="Open Site" href="http://www.deliverycode.com/"><img align="middle" alt="Open Site" class="fwimage" src="http://gallery.mailchimp.com/be5f319b883fe2a2b83f96a61/images/35798292-f9d9-4aff-a679-6f17c68d04c3.jpg" style="display: block;max-width: 398px;width: 100%;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;height: auto;" /></a></td>
+                                                            </tr>
+                                                            <tr>
+                                                              <td style="height: 10px;border-collapse: collapse;" valign="top">
+                                                                &nbsp;</td>
+                                                            </tr>
+                                                            <tr>
+                                                              <td style="border-collapse: collapse;" valign="top">
+                                                                <p style="mso-table-lspace:0;mso-table-rspace:0; margin:0"><span style="color: #000000; font-family: Arial; font-size: 13px; line-height: 16px; padding: 0 0 10px 0; font-weight:bold;">- DeliveryCode&#153; Team</span></p>
+                                                              </td>
+                                                            </tr>
+                                                          </tbody>
+                                                        </table>
+                                                        <!--[if (gte mso 9)|(IE)]> </td>
+                                                                </tr>
+                                                              </table>
+                                                              <![endif]--></td>
+                                                    </tr>
+                                                  </tbody>
+                                                </table>
+                                              </td>
+                                              <td style="width: 20px;border-collapse: collapse;" valign="top">
+                                                &nbsp;</td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                        <!--[if (gte mso 9)|(IE)]> </td>
+                                                  </tr>
+                                                </table>
+                                                <![endif]--></td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style="padding: 10px 10px 10px 10px;border-collapse: collapse;" valign="top">
+                        <!--[if (gte mso 9)|(IE)]>
+                                <table width="580" align="left" cellpadding="0" cellspacing="0" border="0">
+                                  <tr>
+                                    <td>
+                                      <![endif]--><!-- Start Header-->
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" class="deviceWidth" width="580">
+                          <tbody>
+                            <tr>
+                              <td bgcolor="#ffffff" width="100%">
+                                <!-- Social -->
+                                <table align="right" border="0" cellpadding="0" cellspacing="0" class="deviceWidth">
+                                  <tbody>
+                                    <tr>
+                                      <td class="center" style="font-size: 12px; color: #000000; font-weight: bold; text-align: right; font-family: Arial; line-height: 20px; vertical-align: middle; padding:10px 20px; font-style:bold" width="30">
+                                        <a href="#" style="text-decoration: none;"><img align="left" class="fwimage" src="http://gallery.mailchimp.com/be5f319b883fe2a2b83f96a61/images/01ab8850-b625-41f9-a138-86f72bccfc01.jpg" style="display: block;max-width: 31px;width: 100%;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;height: auto;padding-right:20px;" /></a></td>
+                                      <td class="center" style="font-size: 12px; color: #000000; font-weight: bold; text-align: right; font-family: Arial; line-height: 20px; vertical-align: middle; padding:10px 20px; font-style:bold" width="30">
+                                        <a href="#" style="text-decoration: none;"><img align="left" class="fwimage" src="http://gallery.mailchimp.com/be5f319b883fe2a2b83f96a61/images/4ae8f912-7c8e-4a40-b507-631e8c3ae188.jpg" style="display: block;max-width: 31px;width: 100%;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;height: auto;padding-right:20px;" /></a></td>
+                                      <td class="center" style="font-size: 12px; color: #000000; font-weight: bold; text-align: right; font-family: Arial; line-height: 20px; vertical-align: middle; padding:10px 20px; font-style:bold" width="30">
+                                        <a href="#" style="text-decoration: none;"><img align="left" class="fwimage" src="http://gallery.mailchimp.com/be5f319b883fe2a2b83f96a61/images/f7688b13-938c-42f9-a9ec-2cf2b373e0b0.jpg" style="display: block;max-width: 31px;width: 100%;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;height: auto;" /></a></td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                                <!-- End Nav --></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <!-- End Header --><!--[if (gte mso 9)|(IE)]> </td>
+                                  </tr>
+                                </table>
+                                <![endif]--></td>
+                    </tr>
+                    <tr>
+                    </tr>
+                  </tbody>
+                </table>
+
