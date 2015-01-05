6bb3fa9c06c4507a03f42604a41c8b98db11bd3c
Paul Sebborn <paul@zoocha.com>
2015-01-05 16:59:14 +0000

    added instagram to social media

--- sites/all/themes/hiddendelivery/templates/webforms/webform-form-7.tpl.php
+++ sites/all/themes/hiddendelivery/templates/webforms/webform-form-7.tpl.php
@@ -10,8 +10,9 @@
 	</div>
 	<div class="col-md-6">
 		<div class="social-links">
-			<p class="social-links-title">Or contact us via social media: </p>
+			<p class="social-links-title">Or contact us <br/>via social media: </p>
 			<div class="social-icons">
+				<a class="bg-sprite-circle-instagram inverted bg-sprite block-sprite-fixed" href="https://www.instagram.com/deliverycode" target="_blank">DeliveryCode on Instagram </a>
 				<a class="bg-sprite-circle-facebook bg-sprite block-sprite-fixed" href="https://www.facebook.com/deliverycode" target="_blank">DeliveryCode on Facebook</a>
 				<a class="bg-sprite-circle-twitter bg-sprite block-sprite-fixed" href="http://twitter.com/delivery_code" rel="nofollow">DeliveryCode on Twitter"></a>
 				<a class="bg-sprite-circle-pinterest bg-sprite block-sprite-fixed" href="http://pinterest.com/deliverycode" rel="nofollow">DeliveryCode on Twitter"></a>
