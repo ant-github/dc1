35f72772550a812be17ecdf153466003ed34bfd1
Paul <paul@zoocha.com>
2015-01-30 12:22:23 +0000

    fixed reg form styling bugs and onblur for product details

--- sites/all/modules/custom/hd_jeditable/drupal_hd_jeditable.js
+++ sites/all/modules/custom/hd_jeditable/drupal_hd_jeditable.js
@@ -6,12 +6,14 @@
         tooltip   : 'Click to edit...',
         cancel    : 'Cancel',
         submit    : 'Save',
+        onblur    : 'submit',
         style     : 'display: inherit; min-width: 100px;'
       });
       $('.hd-jeditable-textarea', context).editable('/hd_jeditable/ajax/save', {
         type      : 'textarea',
         cancel    : 'Cancel',
         submit    : 'OK',
+        onblur    : 'submit',
         indicator : 'Saving...',
         tooltip   : 'Click to edit...'
       });
@@ -19,6 +21,7 @@
         loadurl  : '/hd_jeditable/ajax/load',
         type     : 'select',
         submit   : 'OK',
+        onblur   : 'submit',
         style    : 'display: inherit'
       });
     }
