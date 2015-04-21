e873c3460fa596c5fbabac90e40b0e1a3618d45b
Soni kamal <sonikamal@antech.in>
2015-04-21 13:30:04 +0530

    changes made for purchased by field in purchases for me view

---
+++ sites/all/themes/hiddendelivery/templates/views/views-view-field--purchases--page--nothing.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+?>
+<?php 
+$purchasedById = $row->_field_data['field_commerce_produc_ref_commerce_product_wishlist_item_id']['entity']->field_reserving_user['und'][0]['target_id'];
+$getUsername = user_load($purchasedById); 
+print ucfirst($getUsername->name);
+//print $output; 
+?>
