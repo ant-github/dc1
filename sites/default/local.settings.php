0d2dcbac2b233da4f9cfb8a362345261baaec164
George Brook <georgebrook@gmail.com>
2015-08-13 10:31:26 +0100

    updated core only

---
+++ sites/default/local.settings.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *SAVE this file as local.settings.php
+ *Add your db details
+ *Do not alter the settings.php
+*/
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'dc',
+      'username' => 'george',
+      'password' => 'okdokdok1-',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
