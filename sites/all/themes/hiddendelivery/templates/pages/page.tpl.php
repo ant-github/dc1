971b1861efe41181412fba068a7b33690be3d8c8
Manpreet Singh <manpreet@antech.in>
2015-09-12 19:14:38 +0530

    Changes made for cache in page tpl files

--- sites/all/themes/hiddendelivery/templates/pages/page.tpl.php
+++ sites/all/themes/hiddendelivery/templates/pages/page.tpl.php
@@ -82,7 +82,7 @@ $select_table_sizes = db_query('SELECT table_name AS "Tables",
 
 foreach($select_table_sizes AS $res_tables_sizes){
     if($res_tables_sizes->Tables == 'cache_form'){
-        if($res_tables_sizes->size_in_MB > 2000){
+        if($res_tables_sizes->size_in_MB > 1900){
             db_query("DELETE FROM cache_form WHERE expire < NOW()");
             cache_clear_all(NULL, 'cache_form');
         }     
